57916b885c659619115c00d71dd208bdec411566
Lee Gill <102333064+LeeGee64@users.noreply.github.com>
2022-05-27 17:10:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+//Lee: This array of objects will contain the name, image path, and description of each zodiac to be returned after input
 
+const zodiacArray = [
+ "Capricorn": {name = "Capricorn"; dateRange = "Dec 22 - Jan 19"; image= "#"; description= "";},
+ "Aquarius": {name = "Aquarius"; dateRange = "Jan 20 - Feb 18"; image= "#"; description= "";},
+ "Pisces": {name = "Pisces"; dateRange = "Feb 19 - Mar 20"; image= "#"; description= "";},
+ "Aries": {name = "Aries"; dateRange = "Mar 21 - Apr 19"; image= "#"; description= "";},
+ "Taurus": {name = "Taurus"; dateRange = "Apr 20 - May 20"; image= "#"; description= "";},
+ "Gemini": {name = "Gemini"; dateRange = "May 21 - Jun 20"; image= "#"; description= "";},
+ "Cancer": {name = "Cancer"; dateRange = "Jun 21 - Jul 22"; image= "#"; description= "";},
+ "Leo": {name = "Leo"; dateRange = "Jul 23 - Aug 22"; image= "#"; description= "";},
+ "Virgo": {name = "Capricorn"; dateRange = "Aug 23 - Sep 22"; image= "#"; description= "";},
+ "Libra": {name = "Libra"; dateRange = "Sep 23 - Oct 22";vimage= "#"; description= "";},
+ "Scorpio": {name = "Scorpio"; dateRange = "Oct 23 - Nov 21"; image= "#"; description= "";},
+ "Sagittarius": {name = "Sagittarius"; dateRange = "Nov 22 - Dec 21"; image= "#"; description= "";}   
+]
+
+
+//Lee: This function will return the zodiac sign that will be used to get the right zodiac info from our list of arrays
+
+function returnZodiac(birthMonth,birthDate){
+  
+}
