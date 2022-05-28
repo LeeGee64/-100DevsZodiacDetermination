98e7074d32a907f4119c24468966c5211b986db4
MaryShodiya <marysho57@gmail.com>
2022-05-28 15:18:55 +0100

    modified main-js file

--- js/main.js
+++ js/main.js
@@ -1,32 +1,146 @@
 //Lee: This array of objects will contain the name, image path, and description of each zodiac to be returned after input
 
-const zodiacArray = [
- "Capricorn": {name = "Capricorn"; dateRange = "Dec 22 - Jan 19"; image= "#"; description= "";},
- "Aquarius": {name = "Aquarius"; dateRange = "Jan 20 - Feb 18"; image= "#"; description= "";},
- "Pisces": {name = "Pisces"; dateRange = "Feb 19 - Mar 20"; image= "#"; description= "";},
- "Aries": {name = "Aries"; dateRange = "Mar 21 - Apr 19"; image= "#"; description= "";},
- "Taurus": {name = "Taurus"; dateRange = "Apr 20 - May 20"; image= "#"; description= "";},
- "Gemini": {name = "Gemini"; dateRange = "May 21 - Jun 20"; image= "#"; description= "";},
- "Cancer": {name = "Cancer"; dateRange = "Jun 21 - Jul 22"; image= "#"; description= "";},
- "Leo": {name = "Leo"; dateRange = "Jul 23 - Aug 22"; image= "#"; description= "";},
- "Virgo": {name = "Capricorn"; dateRange = "Aug 23 - Sep 22"; image= "#"; description= "";},
- "Libra": {name = "Libra"; dateRange = "Sep 23 - Oct 22";vimage= "#"; description= "";},
- "Scorpio": {name = "Scorpio"; dateRange = "Oct 23 - Nov 21"; image= "#"; description= "";},
- "Sagittarius": {name = "Sagittarius"; dateRange = "Nov 22 - Dec 21"; image= "#"; description= "";}   
-]
-
+/*const zodiacArray = {
+  "Capricorn":  {
+      name : "Capricorn", 
+      dateRange :"Dec 22 - Jan 19", 
+      image: "#" , 
+      description: ""
+  },
+  "Aquarius": {
+  name : "Aquarius", 
+  dateRange : "Jan 20 - Feb 18",
+   image: "#" ,
+  description: ""
+  },
+  "Pisces": {
+      name : "Pisces",
+      dateRange : "Feb 19 - Mar 20",
+      image: "#", description: "",
+  },
+  "Aries": {
+  name : "Aries",
+   dateRange : "Mar 21 - Apr 19", 
+   image: "#", 
+   description: ""
+  },
+  "Taurus": {
+      name : "Taurus",
+   dateRange : "Apr 20 - May 20", 
+   image: "#", 
+   description: ""
+  },
+  "Gemini": {
+      name : "Gemini",
+       dateRange : "May 21 - Jun 20",
+        image: "#", 
+        description: ""
+      },
+  "Cancer": {
+      name : "Cancer",
+       dateRange : "Jun 21 - Jul 22", 
+       image: "#",
+      description: ""
+  },
+  "Leo": {
+      name : "Leo",
+       dateRange : "Jul 23 - Aug 22", 
+       image: "#", 
+       description: ""
+      },
+  "Virgo": {
+      name : "Virgo",
+       dateRange : "Aug 23 - Sep 22",
+        image: "#", 
+        description: ""
+      },
+  "Libra": {
+      name : "Libra",
+       dateRange : "Sep 23 - Oct 22",
+       vimage: "#", 
+       description: ""
+      },
+  "Scorpio": 
+  {name : "Scorpio", 
+  dateRange : "Oct 23 - Nov 21",
+   image: "#", 
+   description: ""
+  },
+  "Sagittarius": 
+  {name : "Sagittarius", 
+  dateRange : "Nov 22 - Dec 21", 
+  image: "#",
+   description: ""
+  }   
+}*/
 
 //Lee: This function will return the zodiac sign that will be used to get the right zodiac info from our list of arrays
 
-function returnZodiac(birthMonth,birthDate){
-  switch(birthMonth){
-   case "Jan":
-    birthDate <= 19 ? return "Capricorn" : return "Aquarius";
-    break;
-   //Lee: add a case for each other month of the year similar to the above
-   default:
-    alert("Please enter a valid month!");
-  }
+/*let birthDate = ""
+let birthMonth =  ""
+let result =""
+let image = ""
+let description = ""*/
+ 
+function getZodiacInfo (birthDate, birthMonth) {
+if((birthMonth == 12 &&  birthDate >= 22) || (birthMonth == 1 && birthDate <= 19)){
+    console.log("Capricon")
+    image = ""
+    description = ""
+}else if((birthMonth == 1 && birthDate>=20)||(birthMonth == 2 && birthDate<=18)){
+  console.log("Aquarius")
+    image = ""
+    description = ""
+}else if((birthMonth == 2 && birthDate>=19)|| (birthMonth== 3 && birthDate<= 20)){
+ console.log("Pisces")
+    image = ""
+    description = ""
+}else if((birthMonth == 3 && birthDate>=21)|| (birthMonth== 4 && birthDate<= 19)){
+ console.log("Aries")
+    image = ""
+    description = ""
+}else if((birthMonth == 4 && birthDate>=20)|| (birthMonth== 5 && birthDate<= 20)){
+  console.log("Taurus")
+    image = ""
+    description = ""
+}else if((birthMonth == 5 && birthDate>=21)|| (birthMonth== 6 && birthDate<= 20)){
+  console.log("Gemini")
+    image = ""
+    description = ""
+}else if((birthMonth == 6 && birthDate>=21)|| (birthMonth== 7 && birthDate<= 22)){
+  console.log("Cancer")
+    image = ""
+    description = ""
+}else if((birthMonth == 7 && birthDate>=23)|| (birthMonth== 8 && birthDate<= 22)){
+   console.log("Leo")
+    image = ""
+    description = ""
+}else if((birthMonth == 8 && birthDate>=23)|| (birthMonth== 9 && birthDate<= 22)){
+  console.log("Virgo")
+    image = ""
+    description = ""
+}else if((birthMonth == 9 && birthDate>=23)|| (birthMonth== 10 && birthDate<= 22)){
+  console.log("Libra")
+    image = ""
+    description = ""
+}else if((birthMonth == 10 && birthDate>=23)|| (birthMonth== 11 && birthDate<= 21)){
+  console.log("Scorpio")
+        image = ""
+        description = ""
+}else if((birthMonth == 11 && birthDate>=22)|| (birthMonth== 12 && birthDate<= 21)){
+        console.log("Saggaritus")
+        image = ""
+        description = ""
+}
 }
 
+
 //Lee: create some test cases to make sure that switch functions and that Zodiac Array returns the right values
+getZodiacInfo(1, 6)
+getZodiacInfo(6, 8)
+getZodiacInfo(4, 4)
+getZodiacInfo(12, 12)
+getZodiacInfo(1, 7)
+getZodiacInfo(3, 2)
+getZodiacInfo(28, 4)
+getZodiacInfo(31, 12)
